70c3940814ad7be6e23d0483359f4b982dd4b5ea
anggiaintan <anggia.intan.ft22@mail.umy.ac.id>
2023-05-15 16:05:02 +0700

    memanggil prosedur

--- mbrStaticFunc/mbrStaticFunc.cpp
+++ mbrStaticFunc/mbrStaticFunc.cpp
@@ -12,7 +12,7 @@ public:
 	void setID();
 	void printAll();
 
-	static void setNIM(int pNim) { nim = pNim; /*Definisi Function*/ }
+	static void setNim(int pNim) { nim = pNim; /*Definisi Function*/ }
 	static int getNim() { return nim; /*Definisi Function*/ }
 
 	mahasiswa(string pnama) :nama(pnama) { setID(); }
@@ -28,4 +28,22 @@ void mahasiswa::printAll() {
 	cout << "ID = " << id << endl;
 	cout << "Nama = " << nama << endl;
 	cout << endl;
+}
+
+int main() {
+	mahasiswa mhs1("Sri Dadi");
+	mahasiswa mhs2("Budi Jatmiko");
+	mahasiswa::setNim(9); //mengakses nim melalui static member function "setNim"
+	mahasiswa mhs3("Andi Janu");
+	mahasiswa mhs4("Joko Wahono");
+
+	mhs1.printAll();
+	mhs2.printAll();
+	mhs3.printAll();
+	mhs4.printAll();
+
+	cout << "akses dari luar object = " << mahasiswa::getNim << endl; //mengakses nim melalui static member function "getNim"
+	system("pause");
+
+	return 0;
 }
